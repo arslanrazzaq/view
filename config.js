a18417783ef9c55f5e92afe97f12e9de4ca937c4
Muhammad Arslan Razzaq <arslanrazzaq47@gmail.com>
2023-05-01 04:08:08 +0500

    base url change to aws

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 //dev test
-export const BASE_URL = 'http://localhost:8082/api';
+export const BASE_URL = 'http://view-dev.eu-west-2.elasticbeanstalk.com/api';
 //export const BASE_URL_API = 'https://test.proton.api.atomicassets.io';
 export const ACCESS_TOKEN = '?pinataGatewayToken=2GnEEet4OOTZ8ZScfWTUkr92SpmCj_36zVl3Fn8ByaZ16X8skG5WfFTnhUlnv8RG';
 //prod 
